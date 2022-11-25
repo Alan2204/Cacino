e39928546e1c82ef8e80d2e09effee735ba80160
DVBico <dianixvero0606@gmail.com>
2022-11-25 01:52:14 -0600

    Se creo la clase DdContext, se modificó el startup y apsettings, además se realizó una migración

---
+++ Cacino/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cacino.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cacino
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Participante> Participantes { get; set; }
+    }
+}

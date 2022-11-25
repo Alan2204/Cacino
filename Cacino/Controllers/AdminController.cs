4b941b01cec459c62e93b19a48b80265e5e89d9a
Alan <alan_darkstar@hotmail.com>
2022-11-24 23:44:12 -0600

    first commit

---
+++ Cacino/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cacino.Controllers
+{
+    [ApiController]
+    [Route("Admin")]
+    public class AdminController :ControllerBase
+    {
+
+        
+        public AdminController()
+        {
+            
+        }
+        //[HttpPut]
+        //[HttpPost]
+        //[HttpGet]
+        //[HttpDelete]
+    }
+}
